73e639c1035bf5ed75b3b4c8ff275f3d5d469721
burakgecimli <62941438+burakgecimli@users.noreply.github.com>
2022-01-21 18:25:23 +0300

    Update IkinciFragment.kt

--- 2.NavigationComponentKullanimi/3.BottomNavigation/app/src/main/java/com/bugcompany/bottomnavigation/IkinciFragment.kt
+++ 2.NavigationComponentKullanimi/3.BottomNavigation/app/src/main/java/com/bugcompany/bottomnavigation/IkinciFragment.kt
@@ -8,9 +8,7 @@ import android.view.ViewGroup
 
 
 class IkinciFragment : Fragment() {
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
         return inflater.inflate(R.layout.fragment_ikinci, container, false)
     }
 
