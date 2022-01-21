038289614b52eaaf58833244b28b746fd03726ac
burakgecimli <62941438+burakgecimli@users.noreply.github.com>
2022-01-21 18:24:53 +0300

    Update BirinciFragment.kt

--- 2.NavigationComponentKullanimi/3.BottomNavigation/app/src/main/java/com/bugcompany/bottomnavigation/BirinciFragment.kt
+++ 2.NavigationComponentKullanimi/3.BottomNavigation/app/src/main/java/com/bugcompany/bottomnavigation/BirinciFragment.kt
@@ -8,12 +8,7 @@ import android.view.ViewGroup
 
 
 class BirinciFragment : Fragment() {
-
-
-
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
         return inflater.inflate(R.layout.fragment_birinci, container, false)
     }
 
